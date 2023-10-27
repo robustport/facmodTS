afcad22bb7e179b659996f04c8bd1a3ca9bc556f
robustport <dougPCRA@gmail.com>
2023-10-27 16:06:51 -0700

    Created proper facmodTS-package.R

---
+++ R/facmodTS-package.R
@@ -0,0 +1,23 @@
+#' @keywords internal
+#' @import data.table
+#' @importFrom grDevices topo.colors
+#' @importFrom graphics abline arrows axis barplot box boxplot identify legend lines mtext par points polygon segments strheight strwidth text title
+#' @importFrom methods cbind2
+#' @importFrom stats cov dnorm integrate lm mad median pnorm qchisq sd uniroot var qnorm qqnorm
+#' @importFrom utils globalVariables
+#' @importFrom lattice panel.abline xyplot panel.xyplot
+#' @importFrom xts xts as.xts apply.monthly
+#' @importFrom zoo zoo coredata index index<-
+#' @importFrom boot boot
+#' @importFrom PerformanceAnalytics checkData		
+#' @importFrom PortfolioAnalytics create.EfficientFrontier		
+#' @importFrom corpcor make.positive.definite
+#' @importFrom quadprog solve.QP
+#' @importFrom RobStatTM locScaleM lmrobdet.control lmrobdetMM
+#' @importFrom robustbase scaleTau2
+#' @importFrom R.cache loadCache saveCache
+#' @importFrom utils download.file
+
+"_PACKAGE"
+
+globalVariables(c("Date"))
